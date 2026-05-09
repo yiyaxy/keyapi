b82ad94e52adf46da305ded03f5a338215366016
aliez <1623439963@qq.com>
2026-05-09 21:57:59 +0800

    修改base

--- apps/noterx/frontend/vite.config.ts
+++ apps/noterx/frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
   const target = env.VITE_API_PROXY_TARGET || 'http://localhost:8000'
 
   return {
-    base: '/app/',
+    base: '/noterx/',
     plugins: [react()],
     server: {
       proxy: {
